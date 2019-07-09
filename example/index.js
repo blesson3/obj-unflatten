48f3b00b63ec9f9fc5078e9f0b44d3b964cd5161
Matt Blessed <m@ttblessed.com>
2019-07-09 13:17:37 -0400

    add unflatten array number syntax with example

--- example/index.js
+++ example/index.js
@@ -14,3 +14,15 @@ console.log(unflatten({
   , age: 20
 }, "-"));
 // { name: { first: 'Johnny', last: 'B.' }, age: 20 }
+
+// Unflatten arrays
+console.log(unflatten({
+  'name.first': 'Johnny',
+  'name.last': 'B.',
+  age: 20,
+  'phones.0.number': '6639283',
+  'phones.1.number': '7766839405'
+}, "."));
+// { name: { first: 'Johnny', last: 'B.' },
+//   age: 20,
+//   phones: [ { number: '6639283' }, { number: '7766839405' } ] }
